9c8ae999337fce1e5b650d7d45fcf1b03ff6c1f9
Mansi Mehindru <mansimehindru9002@gmail.com>
2020-06-19 22:07:54 -0400

    added Person class

---
+++ COMP123_M2020_LAB04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_LAB04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person mansi = new Person(name:"Mansi", age: 18);
+
+            mansi.SaysHello();
+
+            Console.ReadLine();
+        }
+    }
+}
